d85d6fca525a14560c94719975618d37f18c856b
Kep <KepCreations@gmail.com>
2021-09-16 23:33:18 -0400

    Update comments

--- connect4.js
+++ connect4.js
@@ -1,8 +1,9 @@
-/** Connect Four
+/* Connect Four
  *
  * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
  * column until a player gets four-in-a-row (horiz, vert, or diag) or until
- * board fills (tie)
+ * the board is filled (tie)
+ * 
  */
 
 const WIDTH = 7;
